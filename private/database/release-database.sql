2609f592518ba67685c22aeea39843a9ba45f9ff
Dean Blackborough <dean@g3d-development.com>
2014-06-24 11:46:11 +0100

    Content updates
    1. Updated the default text for form builder tool bar.
    2. Updated the development plan, form options section.

--- private/database/release-database.sql
+++ private/database/release-database.sql
@@ -2,7 +2,7 @@
 SQLyog Enterprise v11.51 (64 bit)
 MySQL - 5.6.14-log : Database - dlayer
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +12,9 @@ MySQL - 5.6.14-log : Database - dlayer
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dlayer` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db495875811` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `dlayer`;
+USE `db495875811`;
 
 /*Table structure for table `designer_color_palette_colors` */
 
