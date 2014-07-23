fbfa9bad4f3a013611522ffe3bb6d21db358a944
Dean Blackborough <dean@g3d-development.com>
2014-07-23 01:15:01 +0100

    Temp
    1. Remove temp path, not required.

--- library/Dlayer/Form/Image/Add.php
+++ library/Dlayer/Form/Image/Add.php
@@ -134,7 +134,6 @@ class Dlayer_Form_Image_Add extends Dlayer_Form_Module_Image
         $image = new Zend_Form_Element_File('image');
         $image->setLabel('Image');
         $image->setDescription('Choose an image to add to the Image library.');
-        $image->setDestination(APPLICATION_PATH . '/tmp/');
         $image->setBelongsTo('params');
         
         $this->elements['image'] = $image;
