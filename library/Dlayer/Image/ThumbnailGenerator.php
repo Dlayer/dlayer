6171a029067abe7edac7c536a7a53bb14bb0f2f2
Dean Blackborough <dean@g3d-development.com>
2014-07-25 14:38:12 +0100

    Thumbnail generator
    1. Working on the thumbnnail generation class.

---
+++ library/Dlayer/Image/ThumbnailGenerator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Creates a thunmail of the requested image, thumbnails can be set to respect 
+* the aspect ratiopn of the orioinal image added padding as necessary
+* 
+* @author Dean Blackborough
+* @copyright G3D Development Limited
+*/
+class Dlayer_Image_ThumbnailGenerator extends Zend_Form 
+{
+    /**
+    * Add a development controller to test this code, use an image that sits 
+    * in a test folder inside public for testing, that way we can easily 
+    * delete it.
+    * 
+    * 
+    */
+}
