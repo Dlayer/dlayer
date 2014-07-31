a52042a5cfacb257d8273824b4e8a22f85969d24
Dean Blackborough <dean@g3d-development.com>
2014-07-31 11:09:18 +0100

    Dev
    Added a development module.

---
+++ application/modules/dev/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Bootsrap for the Development module
+* 
+* @author Dean Blackborough <dean@g3d-development.com>
+* @copyright G3D Development Limited
+*/
+class Dev_Bootstrap extends Zend_Application_Module_Bootstrap 
+{
+    
+}
---
+++ application/modules/dev/controllers/ThumbnailController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* Controller for image library class development
+*
+* @author Dean Blackborough <dean@g3d-development.com>
+* @copyright G3D Development Limited
+*/
+class Dev_ThumbnailController extends Zend_Controller_Action
+{
+    /**
+    * Type hinting for action helpers, hints the property to the code
+    * hinting class which exists in the library
+    *
+    * @var Dlayer_Action_CodeHinting
+    */
+    protected $_helper;
+
+    private $layout;
+
+    /**
+    * Init the controller, run any set up code required by all the actions
+    * in the controller
+    *
+    * @return void
+    */
+    public function init()
+    {
+        $this->_helper->setLayout('dev');
+        
+        // Include js and css files in layout
+        $this->layout = Zend_Layout::getMvcInstance();
+        $this->layout->assign('js_include', array());
+        $this->layout->assign('css_include', array());
+    }
+
+    /**
+    * Overview action for new class
+    *
+    * @return void
+    */
+    public function indexAction()
+    {
+        
+    }
+    
+    /**
+    * Process
+    *
+    * @return void
+    */
+    public function processAction()
+    {
+        
+    }
+    
+    /**
+    * Load new thumbnail
+    * 
+    * @return void
+    */
+    public function viewAction() 
+    {
+        
+    }
+}
