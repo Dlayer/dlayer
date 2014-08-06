e0b44690d7f2ab9f4b74e40f6075cb318812f99c
Dean Blackborough <dean@g3d-development.com>
2014-08-06 16:17:50 +0100

    Crop class
    Crop class in place, now need to test.

--- library/Dlayer/Image/Crop.php
+++ library/Dlayer/Image/Crop.php
@@ -15,38 +15,48 @@ abstract class Dlayer_Image_Crop
     protected $crop_width;
     protected $crop_height; 
     protected $crop_x;
-    protected $crop_y;   
+    protected $crop_y;
+    
+    protected $src_image;
+    protected $cropped_image;
 
     protected $quality;
     
     protected $mime;    
     protected $extension;
     
-    protected $suffix = '-thumb';
+    protected $suffix = '-cropped';
     
     protected $invalid;
     protected $errors  = array();
     
     /**
-    * Set base resizing options, only setting the base resizing option here 
-    * to allow some simple batch processing by repeatedly calling the loadImage 
-    * and resize method
+    * Set the crop options, set here to allow batch processing by repeatedly 
+    * calling the loadImapge and crop method
     * 
-    * @param integer $width Canvas width
-    * @param integer $height Canvas height
+    * @param integer $x_position X position of selection rectangle
+    * @param integer $y_position Y position of selection rectangle
+    * @param integer $width Width of selection rectangle
+    * @param integer $height Height of selected rectangle
     * @param integer $quality Quality or compression level for new image
-    * @param array $canvas_color Canvas background color
-    * @param boolean $maintain_aspect Maintain aspect ratio of image, if set 
-    *                                 to TRUE padding is added around best fit 
-    *                                 resampled image otherwise image is 
-    *                                 stretched to fit
     * @return void|Exception
     */
-    public function __construct($width, $height, $quality, 
-    array $canvas_color=array('r'=>255, 'g'=>255, 'b'=>255), 
-    $maintain_aspect=TRUE) 
+    public function __construct($x_position, $y_position, $width, $height, 
+    $quality) 
     {
-        if(is_int($width) == FALSE || $width < 1) {            
+        if(is_int($x_position) == FALSE || $x_position < 0) {            
+            $this->invalid++;
+            $this->errors[] = 'X position not valid, must be a positive 
+            integer';
+        }
+        
+        if(is_int($y_position) == FALSE || $y_position < 0) {            
+            $this->invalid++;
+            $this->errors[] = 'Y position not valid, must be a positive 
+            integer';
+        }
+        
+        if(is_int($width) == FALSE || $width < 0) {            
             $this->invalid++;
             $this->errors[] = 'Width not valid, must be an integer above 0';
         }
@@ -56,44 +66,15 @@ abstract class Dlayer_Image_Crop
             $this->errors[] = 'Height not valid, must be an integer above 0';
         }
         
-        if($this->colorIndexValid('r', $canvas_color) == FALSE || 
-        $this->colorIndexValid('g', $canvas_color) == FALSE || 
-        $this->colorIndexValid('b', $canvas_color) == FALSE) {
-            $this->invalid++;
-            $this->errors[] = 'Canvas color array invalid, must contain three 
-            indexes, r, g and b each with values between 0 and 255';
-        }
-        
         if($this->invalid == 0) { 
-            $this->width = $width;
-            $this->height = $height;
+            $this->crop_x = $x_position;
+            $this->crop_y = $y_position;
+            $this->crop_width = $width;
+            $this->crop_height = $height;
             $this->quality = $quality;
-            $this->canvas_color = $canvas_color;
-            if($maintain_aspect == TRUE) {
-                $this->maintain_aspect = TRUE;
-            } else {
-                $this->maintain_aspect = FALSE;
-            }
         } else {
-            throw new InvalidArgumentException("Error(s) creating resizer: " . 
+            throw new InvalidArgumentException("Error(s) creating cropper: " . 
             implode(' - ', $this->errors));
-        }
-    }
-    
-    /**
-    * Check to see if the supplied color index is valid
-    * 
-    * @param string $index
-    * @param array Color array to check
-    * @return boolean
-    */
-    private function colorIndexValid($index, array $canvas_color) 
-    {
-        if(array_key_exists($index, $canvas_color) == TRUE && 
-        $canvas_color[$index] >= 0 && $canvas_color[$index] <= 255) {
-            return TRUE;
-        } else {
-            return FALSE;
         }
     }
     
@@ -114,13 +95,13 @@ abstract class Dlayer_Image_Crop
             $this->validateImage();
             
             $this->sourceDimensions();
+            
+            $this->validateCropSelection();
         } else {
             throw new RuntimeException("File couldn't be found, supplied 
             destination: '" . $path . $file . "'");
         }
     }
-    
-    
     
     /**
     * Validate image is of correct mimetype, if not throw exception, called 
@@ -140,11 +121,28 @@ abstract class Dlayer_Image_Crop
     }
     
     /**
-    * Fetch the dimensions for the source image and the aspect ratio. Also 
-    * checks to ensure that the requested sizes aren't larger than the supplied 
-    * image, the resizer does not upscale images
+    * Validate crop selection, checks to ensure that the crop selection fits 
+    * within the source image
     * 
-    * @return void|Exceptioon Writes the values to the src properties
+    * @return void|Exception
+    */
+    protected function validateCropSelection() 
+    {
+        if(($this->crop_x + $this->crop_width) > $this->src_width || 
+        ($this->crop_y + $this->crop_height) > $this->src_height) {
+            throw new RuntimeException("Crop selection does not fit within the 
+            source image, combined crop width: " . 
+            ($this->crop_x + $this->crop_width) . ' height: ' . 
+            ($this->crop_y + $this->crop_height) . 
+            ' source image dimesions - width: ' . $this->src_width . 
+            ' height: ' . $this->src_height);
+        }
+    }
+    
+    /**
+    * Fetch the dimensions for the source image
+    * 
+    * @return void|Exception Writes the values to the src properties
     */
     protected function sourceDimensions() 
     {
@@ -152,155 +150,30 @@ abstract class Dlayer_Image_Crop
         
         $this->src_width = $dimensions[0];
         $this->src_height = $dimensions[1];
-        $this->src_aspect_ratio = $this->src_width / $this->src_height;
-        
-        if($this->width > $this->src_width || 
-        $this->height > $this->src_height) {
-            throw new InvalidArgumentException("Set resizer width or height 
-            are larger then source width or height, the resizer does not 
-            upscale images.");
-        }
     }
     
     /**
-    * Resize, calculate the size for the resized image maintaing aspect ratio 
-    * whilst attempting to get to the requested canvas size. 
+    * Crop the selected image
     * 
     * Although the suffix for the new image can be defined the path cannot be 
     * changed, that is outside the scope of this class, it is down to the 
     * client developer to create directories and then oevrride the save method
     * 
-    * @param string $suffix Suffix for newly created image 
+    * @param string $suffix Suffix for newly cropped 
     * @return void|Exception
     */
-    public function resize($suffix='-thumb') 
+    public function crop($suffix='-cropped') 
     {
         if(strlen(trim($suffix)) > 0) {
             $this->suffix = trim($suffix);
         } else {
             throw new InvalidArgumentException("Suffix must be defined 
-            otherwise newly created image conflit with source image");
-        }
-        
-        if($this->src_aspect_ratio > 1) {
-            $this->resizeLandscape();
-        } else if($this->src_aspect_ratio == 1) {
-            $this->resizeSquare();
-        } else {
-            $this->resizePortrait();
-        }
-        
-        if($this->maintain_aspect == TRUE) {
-            $this->spacingX();
-        
-            $this->spacingY();
-        } else {
-            $this->dest_width = $this->width;
-            $this->dest_height = $this->height;
+            otherwise cropped image will conflict with source image");
         }
         
         $this->create();
     }
-    
-    protected function resizeLandscape() 
-    {
-        // Set width and then calculate height
-        $this->dest_width = $this->width;
-        $this->dest_height = number_format(
-        $this->dest_width / $this->src_aspect_ratio, 0);
-        
-        // If height larger than requested, set and calculate new width
-        if($this->dest_height > $this->height) {
-            $this->dest_height = $this->height;
-            $this->dest_width = number_format(
-            $this->dest_height * $this->src_aspect_ratio, 0);
-        }
-    }
-    
-    protected function resizeSquare() 
-    {
-        if($this->height == $this->width) {
-            // Requesting a sqaure image, set destination sizes, no spacing
-            $this->dest_width = $this->width;
-            $this->dest_height = $this->height;
-        } else if($this->width > $this->height) {
-            // Requested landscapoe image, set height as dimension, will need 
-            // horizontal spacing
-            $this->dest_width = $this->height;
-            $this->dest_height = $this->height;            
-        } else {
-            // Requested portrait image, set width as dimension, will need 
-            // vertical spacing
-            $this->dest_height = $this->width;
-            $this->dest_width = $this->width;
-        }
-    }
-    
-    protected function resizePortrait() 
-    {
-        // Set height and then calculate width
-        $this->dest_height = $this->height;
-        $this->dest_width = number_format(
-        $this->dest_height * $this->src_aspect_ratio, 0);
-        
-        // If width larger than requested, set and calculate new height
-        if($this->dest_width > $this->width) {
-            $this->dest_width = $this->width;
-            $this->dest_height = number_format(
-            $this->dest_width / $this->src_aspect_ratio, 0);
-        }
-    }
-    
-    /**
-    * Calculate the x spacing if the width of the resampled image will be 
-    * smaller than the width defined for the new thumbnail
-    * 
-    * @return void
-    */
-    protected function spacingX() 
-    {
-        $this->spacing_x = 0;
-        
-        if($this->dest_width < $this->width) {
-            $width_difference = $this->width - $this->dest_width;
-            
-            if($width_difference % 2 == 0) {
-                $this->spacing_x = $width_difference / 2;
-            } else {
-                if($width_difference > 1) {
-                    $this->spacing_x = ($width_difference-1) / 2 + 1;
-                } else {
-                    $this->spacing_x = 1;
-                }
-            }
-        }
-    }
-    
-    /**
-    * Calculate the y spacing if the height of the resampled image will be 
-    * smaller than the height defined for the new thumbnail
-    * 
-    * @return void
-    */
-    protected function spacingY() 
-    {
-        $this->spacing_y = 0;
-        
-        if($this->dest_height < $this->height) {
-            $height_difference = $this->width - $this->dest_width;
-            
-            if($height_difference % 2 == 0) {
-                $this->spacing_y = $height_difference / 2;
-            } else {
-                if($height_difference > 1) {
-                    $this->spacing_y = ($height_difference-1) / 2 + 1;
-                } else {
-                    $this->spacing_y = 1;
-                }
-            }
-        }
-    }
-    
+      
     /**
     * Destroy the image resources
     * 
@@ -308,11 +181,11 @@ abstract class Dlayer_Image_Crop
     */
     public function __destruct() 
     {
-        if(isset($this->canvas) == TRUE) {
-            imagedestroy($this->canvas);
+        if(isset($this->cropped_image) == TRUE) {
+            imagedestroy($this->cropped_image);
         }
-        if(isset($this->copy) == TRUE) {
-            imagedestroy($this->copy); 
+        if(isset($this->src_image) == TRUE) {
+            imagedestroy($this->src_image); 
         }
     }
     
---
+++ library/Dlayer/Image/Crop/Jpeg.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* Jpeg cropper
+* 
+* @see Dlayer_Image_Crop
+* 
+* @author Dean Blackborough
+* @copyright G3D Development Limited
+*/
+class Dlayer_Image_Crop_Jpeg extends Dlayer_Image_Crop 
+{
+    protected $mime = 'image/jpeg';
+    protected $extension = '.jpg';
+    
+    /**
+    * Set the crop options, set here to allow batch processing by repeatedly 
+    * calling the loadImage and crop method
+    * 
+    * @param integer $x_position X position of selection rectangle
+    * @param integer $y_position Y position of selection rectangle
+    * @param integer $width Width of selection rectangle
+    * @param integer $height Height of selected rectangle
+    * @param integer $quality Quality for cropped image, value between 1-100
+    * @return void|Exception
+    */
+    public function __construct($x_position, $y_position, $width, $height, 
+    $quality) 
+    {
+        $this->invalid = 0;
+        
+        if(is_int($quality) == FALSE || $quality < 1 || $quality > 100) {
+            $this->invalid++;
+            $this->errors[] = 'Quality must be an integer value between 1 
+            and 100';
+        }
+        
+        parent::__construct($x_position, $y_position, $width, $height, 
+        $quality);
+    }
+    
+    /**
+    * Create canvas, copy image onto canvas and then save the image
+    * 
+    * @return void|Exception
+    */
+    protected function create() 
+    {
+        $this->src_image = imagecreatefromjpeg($this->path . $this->file);
+        
+        $crop_settings = array('x'=>$this->crop_x, 'y'=>$this->crop_y, 
+        'width'=>$this->crop_width, 'height'=>$this->crop_height);
+        
+        $this->cropped_image = imagecrop($this->src_image, $crop_settings);
+        
+        if($this->cropped_image != FALSE) {
+            $result = $this->save();
+            
+            if($result == FALSE) {
+                throw new RuntimeException("Unable to save new image");
+            }
+        } else {
+            throw new RuntimeException("Unable to crop the requested image.");
+        }
+    }
+    
+    /**
+    * Attempt to save the new image, Overriding  this method to change 
+    * destination 
+    * 
+    * @return boolean
+    */
+    protected function save() 
+    {
+        return imagejpeg($this->canvas, $this->path . 
+        str_replace($this->extension, $this->suffix . $this->extension, 
+        $this->file), $this->quality);
+    }
+}
