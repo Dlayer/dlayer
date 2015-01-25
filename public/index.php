1030c5b2e275205a418feabefa432f05eed53f0f
Dean Blackborough <dean@g3d-development.com>
2015-01-25 01:32:15 +0000

    Environment
    1. Set development environment.

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV') 
     || define('APPLICATION_ENV', 
     (getenv('APPLICATION_ENV') ? 
-    getenv('APPLICATION_ENV') : 'production'));
+    getenv('APPLICATION_ENV') : 'development'));
     
 $include = NULL;
 
