8c6fbc1921ceb1b73ccc1f15b35412db7f15a0a7
Dean Blackborough <dean@g3d-development.com>
2015-02-10 16:45:59 +0000

    Merged in development branch
    1. Content manager UI updated, now need tow work on the content tools,
    need to respect the new structure and I need to display some sub tools.

--- public/index.php
+++ public/index.php
@@ -6,14 +6,14 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+	|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
 defined('APPLICATION_ENV') 
-    || define('APPLICATION_ENV', 
-    (getenv('APPLICATION_ENV') ? 
-    getenv('APPLICATION_ENV') : 'development'));
-    
+	|| define('APPLICATION_ENV', 
+	(getenv('APPLICATION_ENV') ? 
+	getenv('APPLICATION_ENV') : 'production'));
+	
 $include = NULL;
 
 if(APPLICATION_ENV == 'production') {
@@ -22,9 +22,9 @@ if(APPLICATION_ENV == 'production') {
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    $include,
-    get_include_path(),
+	realpath(APPLICATION_PATH . '/../library'),
+	$include,
+	get_include_path(),
 )));
 
 /** Zend_Application */
@@ -32,8 +32,8 @@ require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+	APPLICATION_ENV,
+	APPLICATION_PATH . '/configs/application.ini'
 );
 $application->bootstrap()
-            ->run();
+			->run();
