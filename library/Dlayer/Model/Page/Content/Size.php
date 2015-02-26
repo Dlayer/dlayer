f6b19fffb2fcaf118ace1d6893d529c155175647
Dean Blackborough <dean@g3d-development.com>
2015-02-26 01:20:12 +0000

    Size and position
    1. Size can now be set for text content items.

--- library/Dlayer/Model/Page/Content/Size.php
+++ library/Dlayer/Model/Page/Content/Size.php
@@ -25,8 +25,8 @@ class Dlayer_Model_Page_Content_Size extends Zend_Db_Table_Abstract
 				AND content_id = :content_id';
 		$stmt = $this->_db->prepare($sql);
 		$stmt->bindValue(':site_id', $site_id, PDO::PARAM_INT);
-		$stmt->bindValue(':page_id', $site_id, PDO::PARAM_INT);
-		$stmt->bindValue(':content_id', $site_id, PDO::PARAM_INT);
+		$stmt->bindValue(':page_id', $page_id, PDO::PARAM_INT);
+		$stmt->bindValue(':content_id', $content_id, PDO::PARAM_INT);
 		$stmt->execute();
 		
 		$result = $stmt->fetch();
@@ -37,4 +37,52 @@ class Dlayer_Model_Page_Content_Size extends Zend_Db_Table_Abstract
 			return FALSE;
 		}		
 	}
+	
+	/**
+	* Set the size for the given content item
+	* 
+	* @param integer $site_id
+	* @param integer $page_id
+	* @param integer $content_id
+	* @param integer $size
+	* @return void
+	*/
+	public function setSize($site_id, $page_id, $content_id, $size) 
+	{
+		$sql = 'INSERT INTO user_site_page_content_item_size
+				(site_id, page_id, content_id, size) 
+				VALUES 
+				(:site_id, :page_id, :content_id, :size)';
+		$stmt = $this->_db->prepare($sql);
+		$stmt->bindValue(':site_id', $site_id, PDO::PARAM_INT);
+		$stmt->bindValue(':page_id', $page_id, PDO::PARAM_INT);
+		$stmt->bindValue(':content_id', $content_id, PDO::PARAM_INT);
+		$stmt->bindValue(':size', $size, PDO::PARAM_INT);
+		$stmt->execute();				
+	}
+	
+	/**
+	* Update the size for the given content item
+	* 
+	* @param integer $site_id
+	* @param integer $page_id
+	* @param integer $content_id
+	* @param integer $size
+	* @return void
+	*/
+	public function updateSize($site_id, $page_id, $content_id, $size) 
+	{
+		$sql = 'UPDATE user_site_page_content_item_size 
+				SET size = :size 
+				WHERE site_id = :site_id 
+				AND page_id = :page_id 
+				AND content_id = :content_id 
+				LIMIT 1';
+		$stmt = $this->_db->prepare($sql);
+		$stmt->bindValue(':site_id', $site_id, PDO::PARAM_INT);
+		$stmt->bindValue(':page_id', $page_id, PDO::PARAM_INT);
+		$stmt->bindValue(':content_id', $content_id, PDO::PARAM_INT);
+		$stmt->bindValue(':size', $size, PDO::PARAM_INT);
+		$stmt->execute();
+	}	
 }
