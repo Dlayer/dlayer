36c685423e645bfa915cc5f9bd3279ddae12c75a
Dean Blackborough <dean@g3d-development.com>
2015-05-12 11:35:52 +0100

    Code hinting
    1. Updated method declaration.

--- library/Dlayer/View/Codehinting.php
+++ library/Dlayer/View/Codehinting.php
@@ -380,11 +380,9 @@ class Dlayer_View_Codehinting extends Zend_View_Helper_Abstract
 	* divs, these hoold content items, for each content row the content view 
 	* helper is called which in turns generates the page comtent
 	* 
-	* @param boolean $preview If the content row view helper is in preview mode
-	* 	the helper content rows aren't added
 	* @return Dlayer_View_ContentRow
 	*/
-	public function contentRow($preview=FALSE) { }
+	public function contentRow() { }
 	
 	/** 
 	* Content container styles view helper, generates the style string 
