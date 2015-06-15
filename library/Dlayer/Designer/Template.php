6c00567879c5dc02575272e9025ff8eb0fda81f5
Dean Blackborough <dean@g3d-development.com>
2015-06-15 12:52:46 +0100

    Clean up
    1. Removed template styles view helpers.
    2. Removed template styling tables from database.
    3. New version of release database.

--- library/Dlayer/Designer/Template.php
+++ library/Dlayer/Designer/Template.php
@@ -16,7 +16,6 @@ class Dlayer_Designer_Template
     private $styles;
 
     private $model_template;
-    private $model_styles;
 
     /**
     * Initialise the object, run setup methods and set initial properties
@@ -34,7 +33,6 @@ class Dlayer_Designer_Template
         $this->styles = array();
 
         $this->model_template = new Dlayer_Model_View_Template();
-        $this->model_styles = new Dlayer_Model_View_Template_Styles();
     }
 
     /**
@@ -59,48 +57,6 @@ class Dlayer_Designer_Template
     */
     public function styles()
     {
-        $this->backgroundColorStyles();
-
-        $this->borderStyles();
-
         return $this->styles;
-    }
-
-    /**
-    * Fetch all the background color styles for the template and assign the
-    * data to the styles array, if there are no styles we don't assign anything
-    * to the array
-    *
-    * The id of the currently selected div needs to be passed in because if
-    * the div is selected we won't its background color to be the selected
-    * color not the color defined by the user
-    *
-    * @return void Writes the data to the styles array if values exist
-    */
-    private function backgroundColorStyles()
-    {
-        $styles = $this->model_styles->backgroundColors($this->site_id,
-        $this->template_id, $this->div_id);
-
-        if($styles != FALSE) {
-            $this->styles['background_colors'] = $styles;
-        }
-    }
-
-    /**
-    * Fetch all the border stylesstyles for the template and assign the
-    * data to the styles array, if there are no styles we dont assign anything
-    * to the array
-    *
-    * @return void Writes the data to the styles array if values exist
-    */
-    private function borderStyles()
-    {
-        $styles = $this->model_styles->borders($this->site_id,
-        $this->template_id);
-
-        if($styles != FALSE) {
-            $this->styles['borders'] = $styles;
-        }
     }
 }
