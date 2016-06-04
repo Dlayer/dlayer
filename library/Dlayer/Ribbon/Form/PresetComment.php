5438d6e32ec156212b9f85f16c929f01d39f4519
Dean Blackborough <dean@g3d-development.com>
2016-06-04 11:46:44 +0100

    Comment tool
    - Preset comment tool updated
    - Code style updates

--- library/Dlayer/Ribbon/Form/PresetComment.php
+++ library/Dlayer/Ribbon/Form/PresetComment.php
@@ -1,39 +1,40 @@
 <?php
+
 /**
-* Comment field ribbon data class
-* 
-* Returns the data for the requested tool tab ready top be passed directly 
-* to the tool tab view. The view data method needs to return an array, other 
-* than that there are no specific requirements, it is down to each view script 
-* to handle the data passed to it.
-*
-* @author Dean Blackborough <dean@g3d-development.com>
-* @copyright G3D Development Limited
-* @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
-*/
+ * Name field ribbon data class.
+ *
+ * Returns the data for the requested tool tab ready to be passed to the view script. The viewData method needs to
+ * always return an array, other than that there are no specific requirements for tools
+ *
+ * Each view file needs to check for the expected indexes itself, tools and tool tabs can and do operate differently
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
 class Dlayer_Ribbon_Form_PresetComment extends Dlayer_Ribbon_Module_Form
 {
 	/**
-	* Instantiate and return the form along with any additional data required 
-	* by the tab views
-	*
-	* @param integer $site_id
-	* @param integer $form_id
-	* @param string $tool Name of the selected tool
-	* @param string $tab Name of the selected tool tab
-	* @param integer $multi_use Tool tab multi use param
-	* @param integer|NULL $field_id Id of the form field if in edit mode
-	* @param boolean $edit_mode Is the tool tab in edit mode
-	* @param return array
-	*/
-	public function viewData($site_id, $form_id, $tool, $tab,
-	$multi_use, $field_id=NULL, $edit_mode=FALSE)
+	 * Instantiate and return the form for the tool
+	 *
+	 * @param integer $site_id
+	 * @param integer $form_id
+	 * @param string $tool Name of the selected tool
+	 * @param string $tab Name of the selected tool tab
+	 * @param integer $multi_use Multi use setting
+	 * @param integer|NULL $field_id Field id if in edit mode
+	 * @param boolean $edit_mode Are we in edit mode
+	 * @return array
+	 */
+	public function viewData($site_id, $form_id, $tool, $tab, $multi_use, $field_id = NULL, $edit_mode = FALSE)
 	{
-		$this->writeParams($site_id, $form_id, $tool, $tab, $multi_use, 
-			$field_id, $edit_mode);
+		$this->writeParams($site_id, $form_id, $tool, $tab, $multi_use, $field_id, $edit_mode);
 
-		return array('form'=>new Dlayer_Form_Form_PresetComment(
-			$this->form_id, array(), $this->edit_mode, $this->multi_use), 
-			'field_id'=>$field_id, 'preview_data'=>FALSE);
+		return array(
+			'form' => new Dlayer_Form_Form_PresetComment('preset-comment', 'textarea', '/form/process/tool/',
+				$this->form_id, $this->fieldData(), $this->edit_mode, $this->multi_use),
+			'field_id' => $field_id,
+			'preview_data' => FALSE,
+		);
 	}
 }
