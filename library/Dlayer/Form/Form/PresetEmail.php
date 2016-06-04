81f0a28d240f2f25771e9f0014eb5a1720ef589c
Dean Blackborough <dean@g3d-development.com>
2016-06-04 11:27:08 +0100

    Email tool updated
    - Preset email tool updated, inherits from new base class
    - Code style updates

--- library/Dlayer/Form/Form/PresetEmail.php
+++ library/Dlayer/Form/Form/PresetEmail.php
@@ -1,125 +1,47 @@
 <?php
 /**
-* Form for the email field tool
-* 
-* Allows the user to quickly add an email field to the form, the user simply 
-* needs to click save, they can still override the preset values if they want, 
-* 
-* @todo In addition to adding the field this tool also defines some validation 
-* and other settings
-*  
-* This form is used for the add and edit version of the tool
-*
-* @author Dean Blackborough
-* @copyright G3D Development Limited
-*/
-class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Module_Form
+ * Form class for the preset name tool, allows the user to add a name field which is simply a text field with a few
+ * options preset, once added it behaves as a text field
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
+class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Form_Tool
 {
 	/**
-	* Set the initial properties for the form
-	*
-	* @param integer $form_id
-	* @param array $field_data Field data array, either an array with all the 
-	* 						   attrubutes and their current value or an array 
-	* 						   with FALSE as the value for each attribute
-	* @param boolean $edit_mode Is the tool currently in edit mode
-	* @param integer $multi_use Multi use param for tool tab
-	* @param array|NULL $options Zend form options data array
-	* @return void
-	*/
-	public function __construct($form_id, array $field_data, $edit_mode=FALSE,
-		$multi_use, $options=NULL)
+	 * Set the properties for the form
+	 *
+	 * @param string $tool Tool name, will match database
+	 * @param string $field_type Field type, will match database
+	 * @param string $post_uri Uri to post form to
+	 * @param integer $form_id Id of the form selected in Form builder
+	 * @param array $field_data An array with a field for every element, the data will either be the current data if 	 * editing or FALSE if adding a new field
+	 * @param boolean $edit_mode Are we in edit mode? Lets us see context for labels etc
+	 * @param boolean $multi_use Is the tool or multi-use, if so tool stays selected after processing
+	 * @param array|NULL $options Zend form options
+	 */
+	public function __construct($tool, $field_type, $post_uri, $form_id, array $field_data, $edit_mode=FALSE,
+		$multi_use=TRUE, $options=NULL)
 	{
-		$this->tool = 'preset-email';
-		$this->field_type = 'email';
+		$this->setTitles('Add <small>Email field</small>');
 
-		parent::__construct($form_id, $field_data, $edit_mode, $multi_use, 
-			$options);
+		$this->setLabels('Add email field', 'Save');
+
+		parent::__construct($tool, $field_type, $post_uri, $form_id, $field_data, $edit_mode, $multi_use, $options);
 	}
 
 	/**
-	* Initialuse the form, sers the url and submit method and then calls the
-	* methods that set up the form
-	*
-	* @return void
-	*/
-	public function init()
-	{
-		$this->setAction('/form/process/tool');
-
-		$this->setMethod('post');
-
-		$this->setUpFormElements();
-
-		$this->validationRules();
-		
-		$legend = 'Add <small>Email field</small>'; 
-		$this->addElementsToForm('email_field', $legend, $this->elements);
-					
-		$this->addDefaultElementDecorators();
-
-		$this->addCustomElementDecorators();
-	}
-
-	/**
-	* Set up all the elements required for the form, these are broken down 
-	* into two sections, hidden elements for the tool and then visible 
-	* elements for the user
-	*
-	* @return void The form elements are written to the private $this->elemnets
-	* 			   array
-	*/
-	protected function setUpFormElements()
-	{
-		$this->toolElements();
-
-		$this->userElements();
-	}
-
-	/**
-	* Set up the tool elements, these are the elements that define the tool and 
-	* store the session values for the designer
-	*
-	* @return void Writes the elements to the private $this->elements array
-	*/
-	private function toolElements()
-	{
-		$form_id = new Zend_Form_Element_Hidden('form_id');
-		$form_id->setValue($this->form_id);
-
-		$this->elements['form_id'] = $form_id;
-
-		$tool = new Zend_Form_Element_Hidden('tool');
-		$tool->setValue($this->tool);
-
-		$this->elements['tool'] = $tool;
-
-		$field_type = new Zend_Form_Element_Hidden('field_type');
-		$field_type->setValue($this->field_type);
-
-		$this->elements['field_type'] = $field_type;
-
-		$multi_use = new Zend_Form_Element_Hidden('multi_use');
-		$multi_use->setValue($this->multi_use);
-		$multi_use->setBelongsTo('params');
-
-		$this->elements['multi_use'] = $multi_use;
-	}
-
-	/**
-	* Set up the user elements, these are the elements that the user interacts 
-	* with to use the tool
-	* 
-	* @return void Writes the elements to the private $this->elements array
-	*/
-	private function userElements()
+	 * Add the user facing elements to the form
+	 *
+	 * @return void The elements are written to the $this->elements private property
+	 */
+	protected function addUserElements()
 	{
 		$label = new Zend_Form_Element_Text('label');
 		$label->setLabel('Label');
-		$label->setAttribs(array('maxlength'=>255, 
-			'placeholder'=>'Your email', 'class'=>'form-control input-sm'));
-		$label->setDescription("Enter the label for the email field, defaulted 
-			to 'Your email'.");
+		$label->setAttribs(array('maxlength'=>255, 'placeholder'=>'Your email', 'class'=>'form-control input-sm'));
+		$label->setDescription("Enter the label for the email field, defaults to 'Your email'.");
 		$label->setBelongsTo('params');
 		$label->setValue('Your email');
 
@@ -127,11 +49,9 @@ class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Module_Form
 
 		$description = new Zend_Form_Element_Textarea('description');
 		$description->setLabel('Description');
-		$description->setAttribs(array('rows'=>2, 'cols'=>30, 
-			'placeholder'=>'Please enter your email', 
+		$description->setAttribs(array('rows'=>2, 'cols'=>30, 'placeholder'=>'Please enter your email',
 			'class'=>'form-control input-sm'));
-		$description->setDescription("Enter the description for the email 
-			field, defaulted to 'Please enter your email address'.");
+		$description->setDescription("Enter the description for the email field, defaults to 'Please enter your email address'.");
 		$description->setBelongsTo('params');
 		$description->setValue('Please enter your email address');
 
@@ -139,11 +59,9 @@ class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Module_Form
 
 		$placeholder = new Zend_Form_Element_Text('placeholder');
 		$placeholder->setLabel('Placeholder text');
-		$placeholder->setAttribs(array('maxlength'=>255, 
-			'placeholder'=>'Enter your email', 
+		$placeholder->setAttribs(array('maxlength'=>255, 'placeholder'=>'Enter your email',
 			'class'=>'form-control input-sm'));
-		$placeholder->setDescription("Set the help text to display for the 
-			email field, defaulted to 'Enter you email'.");
+		$placeholder->setDescription("Set the help text to display for the email field, defaults to 'Enter you email'.");
 		$placeholder->setBelongsTo('params');
 		$placeholder->setValue('Enter your email');
 
@@ -152,10 +70,8 @@ class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Module_Form
 		$size = new Dlayer_Form_Element_Number('size');
 		$size->setLabel('Size');
 		$size->setValue(40);
-		$size->setAttribs(array('maxlength'=>3, 'min'=>0, 
-			'class'=>'form-control input-sm'));
-		$size->setDescription('Set the size of the email field in characters,
-			we default to 40 characters.');
+		$size->setAttribs(array('maxlength'=>3, 'min'=>0, 'class'=>'form-control input-sm'));
+		$size->setDescription('Set the size of the email field in characters, defaults to 40 characters.');
 		$size->setBelongsTo('params');
 
 		$this->elements['size'] = $size;
@@ -163,30 +79,10 @@ class Dlayer_Form_Form_PresetEmail extends Dlayer_Form_Module_Form
 		$maxlength = new Dlayer_Form_Element_Number('maxlength');
 		$maxlength->setLabel('Max length');
 		$maxlength->setValue(255);
-		$maxlength->setAttribs(array('maxlength'=>3, 'min'=>0, 
-			'class'=>'form-control input-sm'));
-		$maxlength->setDescription('Set the maximum number of characters that
-			can be entered into the email field, we default to 255 characters.');
+		$maxlength->setAttribs(array('maxlength'=>3, 'min'=>0, 'class'=>'form-control input-sm'));
+		$maxlength->setDescription('Set the maximum number of characters that can be entered into the email field, defaults to 255 characters.');
 		$maxlength->setBelongsTo('params');
 
 		$this->elements['maxlength'] = $maxlength;
-
-		$submit = new Zend_Form_Element_Submit('submit');
-		$submit->setAttrib('class', 'submit');
-		$submit->setAttribs(array('class'=>'btn btn-primary'));
-		$submit->setLabel('Add email field');
-
-		$this->elements['submit'] = $submit;
-	}
-
-	/**
-	* Add the validation rules for the form elements and set the custom error
-	* messages
-	*
-	* @return void
-	*/
-	protected function validationRules()
-	{
-
 	}
 }
