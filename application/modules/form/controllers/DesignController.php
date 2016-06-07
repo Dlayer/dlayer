c2b3fa7ad5bb8b92e47031a660b9af808779ac0f
Dean Blackborough <dean@g3d-development.com>
2016-06-07 15:55:17 +0100

    Preview Javascript
    - Relocated the preview Javascript for the Form builder, updates the
    Text tool, be simpler to turn the feature on and off now that the
    Javascript is separate.
    - Code style updates along the way, also added comments to clear up what
    the Javascript is doing in view script

--- application/modules/form/controllers/DesignController.php
+++ application/modules/form/controllers/DesignController.php
@@ -53,6 +53,7 @@ class Form_DesignController extends Zend_Controller_Action
 				'scripts/dlayer.js',
 				'scripts/designer.js',
 				'scripts/form-builder.js',
+				'scripts/preview-form-builder.js',
 			)
 		);
 		$this->layout->assign('css_include',
