dc935fd8f0d9cdd72851301ffd363a561f10d0c1
Dean Blackborough <dean@g3d-development.com>
2016-08-02 00:04:52 +0100

    Dlayer_Ribbon_Content
    - Added new base class for content manager ribbon classes, simply
    contains three abstract methods at the moment, will increase in
    functionality once we get to the sub tools. [Delivers #126936961]

--- library/Dlayer/Ribbon/Content.php
+++ library/Dlayer/Ribbon/Content.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * Base ribbon data class for ribbon tools, returns all the data required to generate the tool tab views.
- * The contents of a ribbon data class depend on the tool, the only stipulations being that the tool ribbon class
- * implements the abstract methods
+ * Base class for content manager ribbons
  *
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright G3D Development Limited
@@ -11,71 +9,29 @@
  */
 abstract class Dlayer_Ribbon_Content
 {
-	protected $site_id;
-	protected $page_id;
-	protected $div_id;
 	protected $tool;
-	protected $tab;
-	protected $multi_use;
-	protected $edit_mode;
-	protected $content_row_id;
-	protected $content_id;
 
 	/**
-	 * Data method foe the content manager tools, returns the data required
-	 * by the view
+	 * Fetch the view data for the current tool tab, typically the returned array will have at least two indexes,
+	 * one for the form and another with the data required by the preview functions
 	 *
-	 * @param integer $site_id
-	 * @param integer $page_id
-	 * @param integer $div_id
-	 * @param string $tool
-	 * @param string $tab
-	 * @param integer $multi_use
-	 * @param boolean $edit_mode
-	 * @param integer|NULL $content_row_id
-	 * @param integer|NULL $content_id
-	 * @return array|FALSE Either a data array for the tool tab view script or
-	 *    FALSE if no data is required
+	 * @param array $tool Tool and environment data
+	 * @return array
 	 */
-	abstract public function viewData($site_id, $page_id, $div_id, $tool,
-		$tab, $multi_use, $edit_mode = FALSE, $content_row_id = NULL,
-		$content_id = NULL);
+	abstract protected function viewData(array $tool);
 
 	/**
-	 * Take the supplied params and write their values to the internal
-	 * properties
-	 *
-	 * @param integer $site_id
-	 * @param integer $page_id
-	 * @param integer $div_id
-	 * @param string $tool
-	 * @param string $tab
-	 * @param integer $multi_use
-	 * @param boolean $edit_mode
-	 * @param integer|NULL $content_row_id
-	 * @param integer|NULL $content_id
-	 * @return void
-	 */
-	protected function writeParams($site_id, $page_id, $div_id, $tool, $tab,
-		$multi_use, $edit_mode, $content_row_id, $content_id)
-	{
-		$this->site_id = $site_id;
-		$this->page_id = $page_id;
-		$this->div_id = $div_id;
-		$this->tool = $tool;
-		$this->tab = $tab;
-		$this->multi_use = $multi_use;
-		$this->edit_mode = $edit_mode;
-		$this->content_row_id = $content_row_id;
-		$this->content_id = $content_id;
-	}
-
-	/**
-	 * Fetch the data for the content item, the array will either have the
-	 * values for the content item if in edit mode or just the keys with
-	 * FALSE as the valkue if the tool is in add mode
+	 * Fetch the data array for the content item, if in edit mode mode populate the values otherwise every value is
+	 * set to FALSE, the tool form can simply check to see if the value is FALSe or not and then set the existing value
 	 *
 	 * @return array
 	 */
-	abstract protected function contentItem();
+	abstract protected function contentData();
+
+	/**
+	 * Fetch the number of instances for the content items data
+	 *
+	 * @return integer
+	 */
+	abstract protected function instancesOfData();
 }
--- library/Dlayer/Ribbon/Content/Text.php
+++ library/Dlayer/Ribbon/Content/Text.php
@@ -3,15 +3,12 @@
 /**
  * Text content item ribbon data class, fetches all the data required by the base tool tab
  *
- * @todo Need to extend a base class when we have it working again, also need to add the code for preview mode and editing content items
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright G3D Development Limited
  * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
  */
-class Dlayer_Ribbon_Content_Text
+class Dlayer_Ribbon_Content_Text extends Dlayer_Ribbon_Content
 {
-	protected $tool;
-
 	/**
 	 * Fetch the view data for the tool tab, contains an index with the form (pre filled if necessary) and another
 	 * with the data required by the live preview functions
