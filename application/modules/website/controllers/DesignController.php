ba79a679057dce020818d2ea06003f1d00ee7b89
Dean Blackborough <dean@g3d-development.com>
2016-09-24 15:14:50 +0100

    Web site manager
    - Updating web site manager to support modular tools

--- application/modules/website/controllers/DesignController.php
+++ application/modules/website/controllers/DesignController.php
@@ -1,37 +1,42 @@
 <?php
+
 /**
-* The design controller is the root of the web site manager, this is where
-* the user manages the relationships between pages and also manages the site 
-* level data for widgets
-*
-* The manager has a tool bar to the right that shows all the tools for the
-* selected page and a options section underneath for all the controls
-*
-* @author Dean Blackborough <dean@g3d-development.com>
-* @copyright G3D Development Limited
-* @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
-*/
+ * The design controller is the root of the web site manager, this is where
+ * the user manages the relationships between pages and also manages the site
+ * level data for widgets
+ *
+ * The manager has a tool bar to the right that shows all the tools for the
+ * selected page and a options section underneath for all the controls
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
 class Website_DesignController extends Zend_Controller_Action
 {
 	/**
-	* Type hinting for action helpers, hints the property to the code
-	* hinting class which exists in the library
-	*
-	* @var Dlayer_Action_CodeHinting
-	*/
+	 * Type hinting for action helpers, hints the property to the code
+	 * hinting class which exists in the library
+	 *
+	 * @var Dlayer_Action_CodeHinting
+	 */
 	protected $_helper;
 
 	private $session_dlayer;
+
+	/**
+	 * @var Dlayer_Session_Website
+	 */
 	private $session_website;
 
 	private $layout;
 
 	/**
-	* Initialise the controller, run any required set up code and set
-	* properties required by controller actions
-	*
-	* @return void
-	*/
+	 * Initialise the controller, run any required set up code and set
+	 * properties required by controller actions
+	 *
+	 * @return void
+	 */
 	public function init()
 	{
 		$this->_helper->authenticate();
@@ -46,88 +51,102 @@ class Website_DesignController extends Zend_Controller_Action
 		// Include js and css files in layout
 		$this->layout = Zend_Layout::getMvcInstance();
 		$this->layout->assign('js_include', array('scripts/dlayer.js'));
-		$this->layout->assign('css_include', 
+		$this->layout->assign('css_include',
 			array(
-				'css/dlayer.css', 
+				'css/dlayer.css',
 				'css/designer-970.css',
 			)
 		);
 	}
 
 	/**
-	* Base action for the designer controller, loads in the html for the
-	* menu, ribbon, modes, toolbar and template
-	*
-	* @return void
-	*/
+	 * Base action for the designer controller, loads in the html for the
+	 * menu, ribbon, modes, toolbar and template
+	 *
+	 * @return void
+	 */
 	public function indexAction()
 	{
 		$this->navBar('/website/index/index');
 		$this->view->dlayer_toolbar = $this->dlayerToolbar();
 		$this->view->dlayer_website = $this->dlayerWebsite();
 		$this->view->dlayer_ribbon = $this->dlayerRibbon();
-		
+
 		$this->layout->assign('title', 'Dlayer.com - Web site manager');
 	}
 
 	/**
-	* Assign the content for the nav bar
-	* 
-	* @param string $active_uri Uri
-	* @return void Assigns values to the layout
-	*/
-	private function navBar($active_uri) 
+	 * Assign the content for the nav bar
+	 *
+	 * @param string $active_uri Uri
+	 *
+	 * @return void Assigns values to the layout
+	 */
+	private function navBar($active_uri)
 	{
 		$items = array(
-			array('uri'=>'/dlayer/index/home', 'name'=>'Dlayer Demo', 
-				'title'=>'Dlayer.com: Web development simplified'),
-			array('uri'=>'/website/index/index', 
-				'name'=>'Web site manager', 'title'=>'Web site management'), 
-			array('uri'=>'/dlayer/settings/index', 
-				'name'=>'Settings', 'title'=>'Settings'), 
-			array('uri'=>'http://www.dlayer.com/docs/', 
-				'name'=>'Docs', 'title'=>'Read the Docs for Dlayer')
+			array(
+				'uri' => '/dlayer/index/home', 'name' => 'Dlayer Demo',
+				'title' => 'Dlayer.com: Web development simplified',
+			),
+			array(
+				'uri' => '/website/index/index',
+				'name' => 'Web site manager', 'title' => 'Web site management',
+			),
+			array(
+				'uri' => '/dlayer/settings/index',
+				'name' => 'Settings', 'title' => 'Settings',
+			),
+			array(
+				'uri' => 'http://www.dlayer.com/docs/',
+				'name' => 'Docs', 'title' => 'Read the Docs for Dlayer',
+			),
 		);
-		
+
 		$this->layout->assign('nav', array(
-			'class'=>'top_nav', 'items'=>$items, 'active_uri'=>$active_uri));		
+			'class' => 'top_nav', 'items' => $items, 'active_uri' => $active_uri,
+		));
 	}
 
 	/**
-	* Generate the html for the tool bar, the enabled tools for the module are
-	* selected and then passed to a view file. The view is generated using
-	* the tools array and then the result is passed back to the index action
-	*
-	* @return string
-	*/
+	 * Generate the html for the tool bar, the enabled tools for the module are
+	 * selected and then passed to a view file. The view is generated using
+	 * the tools array and then the result is passed back to the index action
+	 *
+	 * @return string
+	 */
 	private function dlayerToolbar()
 	{
-		$model_module = new Dlayer_Model_Module();
+		$model_tool = new Dlayer_Model_Tool();
 
 		$this->view->page_id = $this->session_website->pageId();
-		$this->view->tools = $model_module->tools(
-			$this->getRequest()->getModuleName());
+
+		$this->view->tools = $model_tool->tools($this->getRequest()->getModuleName());
 		$this->view->tool = $this->session_website->tool();
 
 		return $this->view->render("design/toolbar.phtml");
+
 	}
 
 	/**
-	* Generate the html for the ribbon, there are three ribbon states,
-	* the initial state, div selected and then tool selected. The contents
-	* of the ribbon are loaded via AJAX, this method just generates the
-	* container html for when a tool is selected and then the html for the
-	* two base states
-	*
-	* @return string
-	*/
+	 * Generate the html for the ribbon, there are three ribbon states,
+	 * the initial state, div selected and then tool selected. The contents
+	 * of the ribbon are loaded via AJAX, this method just generates the
+	 * container html for when a tool is selected and then the html for the
+	 * two base states
+	 *
+	 * @return string
+	 */
 	private function dlayerRibbon()
 	{
 		$tool = $this->session_website->tool();
 
-		if($tool != FALSE) {
+		if($tool != FALSE)
+		{
 			$html = $this->dlayerRibbonHtml($tool['tool'], $tool['tab']);
-		} else {
+		}
+		else
+		{
 			$ribbon = new Dlayer_Ribbon();
 			$html = $this->view->render($ribbon->defaultViewScriptPath());
 		}
@@ -138,27 +157,31 @@ class Website_DesignController extends Zend_Controller_Action
 	}
 
 	/**
-	* Generate the container html for a tool ribbon tab, view pulls the
-	* tabs for the tool and then generates the tab bar and container. The
-	* contents of the ribbon are loaded via AJAX
-	*
-	* @param string $tool
-	* @param string $tab
-	* @return string
-	*/
+	 * Generate the container html for a tool ribbon tab, view pulls the
+	 * tabs for the tool and then generates the tab bar and container. The
+	 * contents of the ribbon are loaded via AJAX
+	 *
+	 * @param string $tool
+	 * @param string $tab
+	 *
+	 * @return string
+	 */
 	private function dlayerRibbonHtml($tool, $tab)
 	{
 		$ribbon = new Dlayer_Ribbon();
 
 		$tabs = $ribbon->tabs($this->getRequest()->getModuleName(), $tool);
 
-		if($tabs != FALSE) {
+		if($tabs != FALSE)
+		{
 			$this->view->tab = $tab;
 			$this->view->tool = $tool;
 			$this->view->tabs = $tabs;
 			$this->view->module = $this->getRequest()->getModuleName();
 			$html = $this->view->render($ribbon->dynamicViewScriptPath());
-		} else {
+		}
+		else
+		{
 			$html = $this->view->render($ribbon->defaultViewScriptPath());
 		}
 
@@ -166,61 +189,76 @@ class Website_DesignController extends Zend_Controller_Action
 	}
 
 	/**
-	* Generate the html for the requested tool tab, called via AJAX by the
-	* base view.
-	*
-	* The tool and tab are checked to see if they are valid and then the
-	* daata required to generate the tab is pulled and passed to the view.
-	*
-	* @return string
-	*/
+	 * Generate the html for the requested tool tab, called via AJAX by the
+	 * base view.
+	 *
+	 * The tool and tab are checked to see if they are valid and then the
+	 * daata required to generate the tab is pulled and passed to the view.
+	 *
+	 * @return string
+	 */
 	public function ribbonTabHtmlAction()
 	{
 		$this->_helper->disableLayout();
 
-		$tool = $this->getRequest()->getParam('tool');
-		$tab = $this->getRequest()->getParam('tab');
 		$module = $this->getRequest()->getModuleName();
+		$tool = $this->getParamAsString('tool');
+		$sub_tool = $this->getParamAsString('sub_tool');
+		$tab = $this->getParamAsString('tab');
 
-		if($tab != NULL && $tool != NULL) {
+		if($tool !== NULL && $tab !== NULL)
+		{
+			$model_tool = new Dlayer_Model_Tool();
 
-			$ribbon = new Dlayer_Ribbon();
-			$ribbon_tab = new Dlayer_Ribbon_Tab();
+			$exists = $model_tool->tabExists($this->getRequest()->getModuleName(), $tool, $tab);
 
-			$view_script = $ribbon_tab->viewScript(
-				$this->getRequest()->getModuleName(), $tool, $tab);
-			$multi_use = $ribbon_tab->multiUse($module, $tool, $tab);
+			if($exists === TRUE)
+			{
+				/**
+				 * @todo Need to remove this class eventually
+				 */
+				$ribbon_tab = new Dlayer_Ribbon_Tab();
 
-			if($view_script != FALSE) {
+				$this->session_website->setRibbonTab($tab, $sub_tool);
 
-				$this->session_website->setRibbonTab($tab);
+				$this->view->data = $ribbon_tab->viewData($module, $tool, $tab,
+					$model_tool->multiUse($module, $tool, $tab), FALSE);
 
-				$this->view->page_id = $this->session_website->pageId();
-				$this->view->data = $ribbon_tab->viewData($module, $tool,
-					$tab, $multi_use);
+				if($sub_tool === NULL)
+				{
+					$this->view->addScriptPath(DLAYER_LIBRARY_PATH . "\\Dlayer\\DesignerTool\\WebsiteManager\\" .
+						$tool . "\\scripts\\");
+				}
+				else
+				{
+					$this->view->addScriptPath(DLAYER_LIBRARY_PATH . "\\Dlayer\\DesignerTool\\WebsiteManager\\" .
+						$tool . "\\SubTool\\" . $sub_tool ."\\scripts\\");
+				}
 
-				$html = $this->view->render(
-					$ribbon->viewScriptPath($view_script));
-			} else {
-				$html = $this->view->render(
-					$ribbon->defaultViewScriptPath());
+				$html = $this->view->render($tab . '.phtml');
 			}
-		} else {
-			$html = $this->view->render($ribbon->defaultViewScriptPath());
+			else
+			{
+				$html = $this->view->render("design\\ribbon\\default.phtml");
+			}
+		}
+		else
+		{
+			$html = $this->view->render("design\\ribbon\\default.phtml");
 		}
 
 		$this->view->html = $html;
 	}
 
 	/**
-	* Generate the html for the web site overview, includes a section for 
-	* un assigned pages and also a section for the pages in the site.
-	* 
-	* The user can only ever see three levels of the site map at one time, 
-	* their selected level, the children and the parents
-	*
-	* @return string
-	*/
+	 * Generate the html for the web site overview, includes a section for
+	 * un assigned pages and also a section for the pages in the site.
+	 *
+	 * The user can only ever see three levels of the site map at one time,
+	 * their selected level, the children and the parents
+	 *
+	 * @return string
+	 */
 	private function dlayerWebsite()
 	{
 		$designer_website = new Dlayer_Designer_Website(
@@ -235,11 +273,11 @@ class Website_DesignController extends Zend_Controller_Action
 	}
 
 	/**
-	* Sets the selected page as the active page, designer then updates to show
-	* the new children and parents
-	* 
-	* @return void
-	*/
+	 * Sets the selected page as the active page, designer then updates to show
+	 * the new children and parents
+	 *
+	 * @return void
+	 */
 	public function setSelectedPageAction()
 	{
 		$this->_helper->disableLayout(FALSE);
@@ -250,47 +288,69 @@ class Website_DesignController extends Zend_Controller_Action
 	}
 
 	/**
-	* Set the tool, validates that the requested tool is valid and then sets
-	* the params in the content session.
-	*
-	* After a tool has been set the view is refreshed, the ribbon and designer
-	* willbe updated based on the selected tool or item
-	*
-	* Unlike all the other tools the cancel tool clears all template session
-	* values before refreshing the view
-	*
-	* @return void
-	*/
+	 * Set the tool, validates that the requested tool is valid and then sets
+	 * the params in the content session.
+	 *
+	 * After a tool has been set the view is refreshed, the ribbon and designer
+	 * willbe updated based on the selected tool or item
+	 *
+	 * Unlike all the other tools the cancel tool clears all template session
+	 * values before refreshing the view
+	 *
+	 * @return void
+	 */
 	public function setToolAction()
 	{
 		$this->_helper->disableLayout(FALSE);
 
 		$tool = $this->getRequest()->getParam('tool');
 
-		if($tool != NULL && strlen($tool) > 0) {
-			if($tool != 'cancel') {
-				if($this->session_website->setTool($tool) == TRUE) {
-					$this->_redirect('/website/design');
-				} else {
+		if($tool != NULL && strlen($tool) > 0)
+		{
+			if($tool !== 'Cancel')
+			{
+				if($this->session_website->setTool($tool) == TRUE)
+				{
+					$this->redirect('/website/design');
+				}
+				else
+				{
 					$this->cancelTool();
 				}
-			} else {
+			}
+			else
+			{
 				$this->cancelTool();
 			}
-		} else {
+		}
+		else
+		{
 			$this->cancelTool();
 		}
 	}
 
 	/**
-	* The cancel tool clears all the currently set web site manager template 
-	* vars, user is returned to the manager after all session data is cleared
-	*
-	* @return void
-	*/
+	 * The cancel tool clears all the currently set web site manager template
+	 * vars, user is returned to the manager after all session data is cleared
+	 *
+	 * @return void
+	 */
 	private function cancelTool()
 	{
 		$this->session_website->clearAll();
-		$this->_redirect('/website/design');
+		$this->redirect('/website/design');
+	}
+
+	/**
+	 * Get a post param
+	 *
+	 * @todo Move this out of controller
+	 * @param string $param
+	 * @param integer|NULL $default
+	 * @return string|NULL
+	 */
+	private function getParamAsString($param, $default = NULL)
+	{
+		return $this->getRequest()->getParam($param, $default);
 	}
 }
--- library/Dlayer/Session/Website.php
+++ library/Dlayer/Session/Website.php
@@ -1,119 +1,128 @@
 <?php
+
 /**
-* Custom session class for the web site module, store vars which we need to
-* manage the environment, attemping to not have visible get vars so this class
-* will deal with the values
-*
-* @author Dean Blackborough <dean@g3d-development.com>
-* @copyright G3D Development Limited
-* @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
-*/
+ * Custom session class for the web site module, store vars which we need to
+ * manage the environment, attemping to not have visible get vars so this class
+ * will deal with the values
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
 class Dlayer_Session_Website extends Zend_Session_Namespace
 {
-    /**
-    * @param string $namespace
-    * @param bool $singleInstance
-    * @return void
-    */
-    public function __construct($namespace = 'dlayer_session_website',
-    $singleInstance = false)
-    {
-        parent::__construct($namespace, $singleInstance);
+	/**
+	 * @param string $namespace
+	 * @param bool $singleInstance
+	 *
+	 * @return void
+	 */
+	public function __construct($namespace = 'dlayer_session_website',
+		$singleInstance = FALSE)
+	{
+		parent::__construct($namespace, $singleInstance);
 
-        $this->setExpirationSeconds(3600);
-    }
+		$this->setExpirationSeconds(3600);
+	}
 
-    /**
-    * Set the id for the page that the user wants to manage
-    *
-    * @todo Need to check that the page id is valid
-    * @param integer $id
-    * @return void
-    */
-    public function setPageId($id)
-    {
-        $this->page_id = intval($id);
-    }
+	/**
+	 * Set the id for the page that the user wants to manage
+	 *
+	 * @todo Need to check that the page id is valid
+	 *
+	 * @param integer $id
+	 *
+	 * @return void
+	 */
+	public function setPageId($id)
+	{
+		$this->page_id = intval($id);
+	}
 
-    /**
-    * Get the id of the page that the user is currently working on
-    *
-    * @return integer|NULL
-    */
-    public function pageId()
-    {
-        return $this->page_id;
-    }
+	/**
+	 * Get the id of the page that the user is currently working on
+	 *
+	 * @return integer|NULL
+	 */
+	public function pageId()
+	{
+		return $this->page_id;
+	}
 
+	/**
+	 * Set the selected tool, before setting we check to see if the requested tool is valid, a tool is a
+	 * combination of a tool model, an option sub tool model and the tab itself
+	 *
+	 * @param string $tool Tool model name
+	 * @return boolean
+	 */
+	public function setTool($tool)
+	{
+		$session_dlayer = new Dlayer_Session();
+		$model_tool = new Dlayer_Model_Tool();
 
-    /**
-    * Set the selected tool, before setting we check to see if the requested
-    * tool is valid, if valid we set the default tab for the tool
-    *
-    * @param string $tool Name of the tool to set
-    * @return boolean
-    */
-    public function setTool($tool)
-    {
-        $session_dlayer = new Dlayer_Session();
-        $model_tool = new Dlayer_Model_Tool();
+		$tool = $model_tool->toolAndDefaultTab($session_dlayer->module(), $tool);
 
-        $tool_details = $model_tool->valid($session_dlayer->module(), $tool);
+		if($tool !== FALSE)
+		{
+			$this->tool = $tool['tool'];
+			$this->setRibbonTab($tool['tab'], $tool['sub_tool']);
 
-        if($tool_details != FALSE) {
-            $this->tool = $tool;
-            $this->tool_model = $tool_details['tool_model'];
-            $this->tool_destructive = $tool_details['destructive'];
-            $this->setRibbonTab($tool_details['tab']);
-            return TRUE;
-        } else {
-            return FALSE;
-        }
-    }
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
 
-    /**
-    * Set the tool tab
-    *
-    * @param string $tab
-    * @return void
-    */
-    public function setRibbonTab($tab)
-    {
-        $this->tab = $tab;
-    }
+	/**
+	 * Set the tool tab
+	 *
+	 * @param string $tab
+	 * @param string|NULL $sub_tool
+	 * @return void
+	 */
+	public function setRibbonTab($tab, $sub_tool=NULL)
+	{
+		$this->tab = $tab;
+		$this->sub_tool = $sub_tool;
+	}
 
-    /**
-    * Returns the data array for the currently selected tool, if no tool is
-    * set the method returns FALSE
-    *
-    * @return array|FALSE Array contains two indexes, tool and tab, name is
-    *                     the name of the tool, tab is the name of the
-    *                     selected tab
-    */
-    public function tool()
-    {
-        if($this->tool != NULL && $this->tab != NULL &&
-        $this->tool_model != NULL) {
-            return array('tool'=>$this->tool,
-                         'tab'=>$this->tab,
-                         'model'=>$this->tool_model, 
-                         'destructive'=>$this->tool_destructive);
-        } else {
-            return FALSE;
-        }
-    }
-    
-    /**
-    * Clears the session values for the web site manager, these are the vars 
-    * that relate to the current state of the designer, selected page, 
-    * tool and tool tab.
-    * 
-    * @return void
-    */
-    public function clearAll($reset=FALSE)
-    {
-        $this->tool = NULL;
-        $this->tab = NULL;
-        $this->page_id = NULL;
-    }
+	/**
+	 * Returns the data array for the currently selected tool, if no tool is set the method returns FALSE, a tool is
+	 * the combination of the tool itself and the selected tab
+	 *
+	 * @return array|FALSE
+	 */
+	public function tool()
+	{
+		if($this->tool !== NULL && $this->tab !== NULL)
+		{
+			return array(
+				'tool' => $this->tool,
+				'sub_tool' => $this->sub_tool, // Sub tool model can be NULL
+				'tab' => $this->tab
+			);
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+
+	/**
+	 * Clears the session values for the web site manager, these are the vars
+	 * that relate to the current state of the designer, selected page,
+	 * tool and tool tab.
+	 *
+	 * @return void
+	 */
+	public function clearAll($reset = FALSE)
+	{
+		$this->tool = NULL;
+		$this->sub_tool = NULL;
+		$this->tab = NULL;
+		$this->page_id = NULL;
+	}
 }
