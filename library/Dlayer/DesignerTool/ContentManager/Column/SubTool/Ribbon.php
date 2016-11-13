8aa7f903bfcf230e62597b084b172c953d5c447c
Dean Blackborough <dean@g3d-development.com>
2016-11-13 15:20:01 +0000

    Column styling
    - Added ribbon and tool tool class
    - Updates base form class, content type optionally set, won't exists for
    all sub tools (Columns, Rows etc)

---
+++ library/Dlayer/DesignerTool/ContentManager/Column/SubTool/Ribbon.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Column styling sub tool ribbon data class
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
+class Dlayer_DesignerTool_ContentManager_Column_SubTool_Styling_Ribbon extends Dlayer_Ribbon_Content
+{
+    /**
+     * Fetch the view data for the current tool tab, typically the returned array will have at least two indexes,
+     * one for the form and another with the data required by the preview functions
+     *
+     * @param array $tool Tool and environment data
+     *
+     * @return array
+     */
+    public function viewData(array $tool)
+    {
+        $this->tool = $tool;
+
+        return array(
+            'form' => new Dlayer_DesignerTool_ContentManager_Column_SubTool_Styling_Form(
+                $tool,
+                $this->contentData(),
+                $this->instancesOfData(),
+                array()
+            ),
+        );
+    }
+
+    /**
+     * Fetch the data array for the column, if in edit mode populate the values otherwise every value is
+     * set to FALSE, the tool form can simply check to see if the value is FALSE, if not it can use the value directly
+     *
+     * @return array
+     */
+    protected function contentData()
+    {
+        $data = array(
+            'background_color' => false,
+        );
+
+        if ($this->tool['content_id'] !== null) {
+            $model_styling = new Dlayer_DesignerTool_ContentManager_Column_SubTool_Styling_Model();
+            $background_color = $model_styling->backgroundColor(
+                $this->tool['site_id'],
+                $this->tool['page_id'],
+                $this->tool['column_id']
+            );
+
+            if ($background_color !== false) {
+                $data['background_color'] = $background_color;
+            }
+
+        }
+
+        return $data;
+    }
+
+    /**
+     * Fetch the number of instances for the content items data
+     *
+     * @return integer
+     */
+    protected function instancesOfData()
+    {
+        return 0;
+    }
+}
---
+++ library/Dlayer/DesignerTool/ContentManager/Column/SubTool/Tool.php
@@ -0,0 +1,173 @@
+a<?php
+
+/**
+ * Styling sub tool
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
+class Dlayer_DesignerTool_ContentManager_Column_SubTool_Styling_Tool extends Dlayer_Tool_Content
+{
+    /**
+     * Check that the required params have been submitted, check the keys in the params array
+     *
+     * @param array $params
+     *
+     * @return boolean
+     */
+    protected function paramsExist(array $params)
+    {
+        $valid = false;
+        if (array_key_exists('background_color', $params) === true) {
+            $valid = true;
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Check to ensure the posted params are of the correct type and optionally within range
+     *
+     * @param array $params
+     *
+     * @return boolean
+     */
+    protected function paramsValid(array $params)
+    {
+        $valid = false;
+        if (strlen(trim($params['background_color'])) === 0 ||
+            (strlen(trim($params['background_color'])) === 7 &&
+                Dlayer_Validate::colorHex($params['background_color']) === true)
+        ) {
+            $valid = true;
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Prepare the posted params, convert them to the required types and assign to the $this->params property
+     *
+     * @param array $params
+     * @param boolean $manual_tool Are the values to be assigned to $this->params or $this->params_auto
+     *
+     * @return void
+     */
+    protected function paramsAssign(array $params, $manual_tool = true)
+    {
+        if (array_key_exists('background_color', $params) === true) {
+            $this->params['background_color'] = trim($params['background_color']);
+        }
+    }
+
+    /**
+     * Validate the instances param, need to see if it should exist first
+     *
+     * @param integer $site_id
+     * @param integer $content_id
+     *
+     * @return boolean
+     */
+    protected function validateInstances($site_id, $content_id)
+    {
+        // TODO: Implement validateInstances() method.
+    }
+
+    /**
+     * Add a new item
+     *
+     * @return array|FALSE Ids for new environment vars or FALSE if the request failed
+     */
+    protected function add()
+    {
+        // TODO: Implement add() method.
+    }
+
+    /**
+     * Edit the item
+     *
+     * @return array|FALSE Ids for new environment vars or FALSE if the request failed
+     * @throws Exception
+     */
+    protected function edit()
+    {
+        $this->backgroundColor();
+
+        return $this->returnIds();
+    }
+
+    /**
+     * Manage the background color for the selected item
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function backgroundColor()
+    {
+        $model_text = new Dlayer_DesignerTool_ContentManager_Column_SubTool_Styling_Model();
+
+        $id = $model_text->existingBackgroundColor($this->site_id, $this->page_id, $this->column_id);
+
+        if ($id === false) {
+            try {
+                $model_text->addBackgroundColor(
+                    $this->site_id,
+                    $this->page_id,
+                    $this->column_id,
+                    $this->params['background_color']
+                );
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage(), $e->getCode(), $e);
+            }
+        } else {
+            try {
+                $model_text->editBackgroundColor($id, $this->params['background_color']);
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage(), $e->getCode(), $e);
+            }
+        }
+    }
+
+    /**
+     * Make a structural change to the page
+     *
+     * @return array|FALSE An array of the environment vars to set or FALSE upon error
+     */
+    protected function structure()
+    {
+        // TODO: Implement structure() method.
+    }
+
+    /**
+     * Generate the return ids array
+     *
+     * @return array
+     */
+    protected function returnIds()
+    {
+        return array(
+            array(
+                'type' => 'page_id',
+                'id' => $this->page_id,
+            ),
+            array(
+                'type' => 'row_id',
+                'id' => $this->row_id,
+            ),
+            array(
+                'type' => 'column_id',
+                'id' => $this->column_id,
+            ),
+            array(
+                'type' => 'tool',
+                'id' => 'Column',
+            ),
+            array(
+                'type' => 'tab',
+                'id' => 'styling',
+                'sub_tool' => 'Styling'
+            )
+        );
+    }
+}
