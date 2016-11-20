a73861f5976838877787994e720554ed3c1e8404
Dean Blackborough <dean@g3d-development.com>
2016-11-20 23:18:51 +0000

    Database
    - Added row background colour table

--- private/database/release-database.sql
+++ private/database/release-database.sql
@@ -1949,6 +1949,27 @@ insert  into `user_site_page_styling_content_item_background_color`(`id`,`site_i
 (2,1,1,2,'#f0ad4e'),
 (3,1,1,4,'#5bc0de');
 
+/*Table structure for table `user_site_page_styling_row_background_color` */
+
+DROP TABLE IF EXISTS `user_site_page_styling_row_background_color`;
+
+CREATE TABLE `user_site_page_styling_row_background_color` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` int(11) unsigned NOT NULL,
+  `page_id` int(11) unsigned NOT NULL,
+  `row_id` int(11) unsigned NOT NULL,
+  `background_color` char(7) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `site_id` (`site_id`),
+  KEY `page_id` (`page_id`),
+  KEY `row_id` (`row_id`),
+  CONSTRAINT `user_site_page_styling_row_background_color_ibfk_1` FOREIGN KEY (`site_id`) REFERENCES `user_site` (`id`),
+  CONSTRAINT `user_site_page_styling_row_background_color_ibfk_2` FOREIGN KEY (`page_id`) REFERENCES `user_site_page` (`id`),
+  CONSTRAINT `user_site_page_styling_row_background_color_ibfk_3` FOREIGN KEY (`row_id`) REFERENCES `user_site_page_structure_row` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user_site_page_styling_row_background_color` */
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
