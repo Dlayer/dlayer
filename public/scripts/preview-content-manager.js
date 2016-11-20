e9d6994b71e33e111f213d072c17a7b5beac1991
Dean Blackborough <dean@g3d-development.com>
2016-11-20 22:47:16 +0000

    Animate colour
    - Swiitched to animating the background colour change.

--- public/scripts/preview-content-manager.js
+++ public/scripts/preview-content-manager.js
@@ -10,16 +10,16 @@ var previewContentManager =
     highlight: false,
     changed: false,
     visible: false,
+    animateDuration: 600,
+    hightlightDuration: 400,
 
     /**
      * Display a message if any data has changed and not yet been saved
      */
-    unsaved: function()
+    unsaved: function ()
     {
-        if(previewContentManager.visible === false)
-        {
-            if(previewContentManager.changed === true)
-            {
+        if (previewContentManager.visible === false) {
+            if (previewContentManager.changed === true) {
                 $('p.unsaved').show('medium');
                 previewContentManager.visible = true;
             }
@@ -30,17 +30,15 @@ var previewContentManager =
      * Add a slight highlight to the item that has just been changed
      *
      * @param {String} selector Element selector
-     * @param {Number} effect_length Length for effect, defaults to 400 if not set
+     * @param {Number} effect_length Length for effect, defaults to hightlightDuration if not set
      */
-    highlightItem: function(selector, effect_length)
+    highlightItem: function (selector, effect_length)
     {
-        if(typeof effect_length == 'undefined')
-        {
-            effect_length = 400
+        if (typeof effect_length == 'undefined') {
+            effect_length = previewContentManager.hightlightDuration;
         }
 
-        if(previewContentManager.highlight === true)
-        {
+        if (previewContentManager.highlight === true) {
             $(selector).effect("highlight", {}, effect_length);
         }
     },
@@ -51,32 +49,25 @@ var previewContentManager =
      * @param {Number} content_id Id of the content item
      * @param {String} value The background color to set
      */
-    contentItemBackgroundColor: function(content_id, value)
+    contentItemBackgroundColor: function (content_id, value)
     {
-        $('#params-content_background_color').change(function()
+        $('#params-content_background_color').change(function ()
         {
             var new_value = this.value;
             var selector = '.content[data-content-id="' + content_id + '"]';
 
-            if(new_value.length === 7)
-            {
-                $(selector).css('background-color', new_value);
+            if (new_value.length === 7) {
+                $(selector).animate({'backgroundColor': new_value}, previewContentManager.animateDuration);
 
                 previewContentManager.changed = true;
-                previewContentManager.highlight = true;
-            }
-            else
-            {
-                if(new_value.length == 0)
-                {
+            } else {
+                if (new_value.length == 0) {
                     $(selector).css('background-color', 'inherit');
                     previewContentManager.changed = true;
-                    previewContentManager.highlight = true;
                 }
             }
 
             previewContentManager.unsaved();
-            previewContentManager.highlightItem(selector);
         });
     },
 
@@ -86,32 +77,25 @@ var previewContentManager =
      * @param {Number} column_id Id of the column
      * @param {String} value The background color to set
      */
-    columnBackgroundColor: function(column_id, value)
+    columnBackgroundColor: function (column_id, value)
     {
-        $('#params-background_color').change(function()
+        $('#params-background_color').change(function ()
         {
             var new_value = this.value;
             var selector = '.column[data-column-id="' + column_id + '"]';
 
-            if(new_value.length === 7)
-            {
-                $(selector).css('background-color', new_value);
+            if (new_value.length === 7) {
+                $(selector).animate({'backgroundColor': new_value}, previewContentManager.animateDuration);
 
                 previewContentManager.changed = true;
-                previewContentManager.highlight = true;
-            }
-            else
-            {
-                if(new_value.length == 0)
-                {
+            } else {
+                if (new_value.length == 0) {
                     $(selector).css('background-color', 'inherit');
                     previewContentManager.changed = true;
-                    previewContentManager.highlight = true;
                 }
             }
 
             previewContentManager.unsaved();
-            previewContentManager.highlightItem(selector);
         });
     }
 };
