d75ef43665769a0bcc1f2b57f3d6fb4cec7a6d72
Dean Blackborough <dean@g3d-development.com>
2017-01-08 16:13:05 +0000

    Form titles
    - Imported form content items now display the defined title and sub
    title.

--- library/Dlayer/Model/View/Page/Item/Form.php
+++ library/Dlayer/Model/View/Page/Item/Form.php
@@ -10,68 +10,52 @@
  */
 class Dlayer_Model_View_Page_Item_Form extends Zend_Db_Table_Abstract
 {
-	/**
-	 * Fetch the core data needed to create a 'heading' based content item
-	 *
-	 * @param integer $site_id
-	 * @param integer $page_id
-	 * @param integer $id Id of the content item
-	 * @return array|FALSE Either the content item data array or FALSE upon error
-	 */
-	private function baseItemData($site_id, $page_id, $id)
-	{
-		$sql = "SELECT uspcif.content_id, uspcif.form_id 
-				FROM user_site_page_content_item_form uspcif  
-				WHERE uspcif.content_id = :content_id  
-				AND uspcif.site_id = :site_id 
-				AND uspcif.page_id = :page_id";
-		$stmt = $this->_db->prepare($sql);
-		$stmt->bindValue(':site_id', $site_id, PDO::PARAM_INT);
-		$stmt->bindValue(':page_id', $page_id, PDO::PARAM_INT);
-		$stmt->bindValue(':content_id', $id, PDO::PARAM_INT);
-		$stmt->execute();
+    /**
+     * Fetch the core data needed to create a 'heading' based content item
+     *
+     * @param integer $site_id
+     * @param integer $page_id
+     * @param integer $id Id of the content item
+     * @return array|FALSE Either the content item data array or FALSE upon error
+     */
+    private function baseItemData($site_id, $page_id, $id)
+    {
+        $sql = "SELECT 
+                    `uspcif`.`content_id`, 
+                    `uspcif`.`form_id`, 
+                    `usfl`.`title`, 
+                    `usfl`.`sub_title`
+				FROM 
+				    `user_site_page_content_item_form` `uspcif` 
+                LEFT JOIN  
+                    `user_site_form_layout` `usfl` ON
+                        `uspcif`.`form_id` = `usfl`.id
+				WHERE 
+				    `uspcif`.`content_id` = :content_id AND 
+				    `uspcif`.`site_id` = :site_id AND 
+				    `uspcif`.`page_id` = :page_id";
+        $stmt = $this->_db->prepare($sql);
+        $stmt->bindValue(':site_id', $site_id, PDO::PARAM_INT);
+        $stmt->bindValue(':page_id', $page_id, PDO::PARAM_INT);
+        $stmt->bindValue(':content_id', $id, PDO::PARAM_INT);
+        $stmt->execute();
 
-		return $stmt->fetch();
-	}
+        return $stmt->fetch();
+    }
 
-	/**
-	 * Fetch the data needed to create a 'form' based content item, this will include all the data that may have
-	 * been defined by any sub tools
-	 *
-	 * @param integer $site_id
-	 * @param integer $page_id
-	 * @param integer $id Id of the content item
-	 * @return array|FALSE Either the content item data or FALSE upon error
-	 */
-	public function data($site_id, $page_id, $id)
-	{
-		$content_item = $this->baseItemData($site_id, $page_id, $id);
+    /**
+     * Fetch the data needed to create a 'form' based content item, this will include all the data that may have
+     * been defined by any sub tools
+     *
+     * @param integer $site_id
+     * @param integer $page_id
+     * @param integer $id Id of the content item
+     * @return array|FALSE Either the content item data or FALSE upon error
+     */
+    public function data($site_id, $page_id, $id)
+    {
+        $content_item = $this->baseItemData($site_id, $page_id, $id);
 
-		return $content_item;
-	}
-
-	/**
-	 * Fetch the title values for the form
-	 *
-	 * @return array|FALSE
-	 */
-	private function titles($site_id, $form_id)
-	{
-		$model_layout = new Dlayer_Model_View_Form_Layout();
-
-		$titles = $model_layout->titles($site_id, $form_id);
-
-		if($titles != FALSE)
-		{
-			return array(
-				'show' => TRUE,
-				'title' => $titles['title'],
-				'sub_title' => $titles['sub_title'],
-			);
-		}
-		else
-		{
-			return array('show' => FALSE);
-		}
-	}
+        return $content_item;
+    }
 }
