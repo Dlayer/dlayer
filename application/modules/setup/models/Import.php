f0b1e91709a22cf1082fc27aa277177ec1b6784d
Dean Blackborough <dean@g3d-development.com>
2017-01-13 16:15:03 +0000

    Testing
    - Messing with import

--- application/modules/setup/models/Import.php
+++ application/modules/setup/models/Import.php
@@ -16,6 +16,27 @@ class Setup_Model_Import extends Zend_Db_Table_Abstract
      */
     public function setForeignKeyChecks($value)
     {
-        return 'SET FOREIGN_KEY_CHECKS = :value;';
+        return 'SET FOREIGN_KEY_CHECKS = :value;' . PHP_EOL;
+    }
+
+    /**
+     * Execute queries
+     *
+     * @param string $query
+     * @param boolean $foreign_keys Modify SET_FOREIGN_KEYS
+     * @return boolean
+     */
+    public function executeQuery($query, $foreign_keys = false)
+    {
+        if ($foreign_keys === true) {
+            $sql = $this->setForeignKeyChecks(0);
+            $sql .= $query . PHP_EOL;
+            $sql .= $this->setForeignKeyChecks(1);
+        } else {
+            $sql = $query;
+        }
+
+        $stmt = $this->_db->prepare($sql);
+        return $stmt->execute();
     }
 }
