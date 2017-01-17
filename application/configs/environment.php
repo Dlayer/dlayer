b66b0c007c95b0b5017679cf0894c2993a473ca7
Dean Blackborough <dean@g3d-development.com>
2017-01-17 13:33:11 +0000

    Version
    - Updated version to 1.05.01

--- application/configs/environment.php
+++ application/configs/environment.php
@@ -14,6 +14,6 @@ $environment = 'production';
 //$environment = 'development';
 
 // Version number for current release
-$version_no = 'v1.05';
+$version_no = 'v1.05.01';
 // Release date for current release
 $version_release_date = '17th January 2017';
