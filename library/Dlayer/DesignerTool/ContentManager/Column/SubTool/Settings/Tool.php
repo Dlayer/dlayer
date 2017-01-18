b888d26583a8c9edaa767e860ed6d2fa63f7f909
Dean Blackborough <dean@g3d-development.com>
2017-01-18 18:38:40 +0000

    Tool class
    - Working on tool class for column settings sub tool.

---
+++ library/Dlayer/DesignerTool/ContentManager/Column/SubTool/Settings/Tool.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * Settings sub tool
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited
+ * @license https://github.com/Dlayer/dlayer/blob/master/LICENSE
+ */
+class Dlayer_DesignerTool_ContentManager_Column_SubTool_Settings_Tool extends Dlayer_Tool_Content
+{
+    /**
+     * @var Dlayer_DesignerTool_ContentManager_Column_SubTool_Settings_Model
+     */
+    private $model;
+
+    /**
+     * @var array
+     */
+    private $current_values = array();
+
+    /**
+     * @var boolean
+     */
+    private $current_values_fetched = false;
+
+    /**
+     * Check that the required params have been submitted, check the keys in the params array
+     *
+     * @param array $params
+     *
+     * @return boolean
+     */
+    protected function paramsExist(array $params)
+    {
+        $valid = false;
+        if (array_key_exists('width', $params) === true && array_key_exists('offset', $params) === true) {
+            $valid = true;
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Check to ensure the posted params are of the correct type and optionally within range
+     *
+     * @param array $params
+     *
+     * @return boolean
+     */
+    protected function paramsValid(array $params)
+    {
+        $valid = false;
+        if (
+            (intval($params['width']) > 0 && intval($params['width']) < 13 === 0)
+            &&
+            intval($params['offset']) >= 0
+        ) {
+            $valid = true;
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Prepare the posted params, convert them to the required types and assign to the $this->params property
+     *
+     * @param array $params
+     * @param boolean $manual_tool Are the values to be assigned to $this->params or $this->params_auto
+     *
+     * @return void
+     */
+    protected function paramsAssign(array $params, $manual_tool = true)
+    {
+        if (array_key_exists('width', $params) === true) {
+            $this->params['width'] = intval($params['width']);
+        }
+
+        if (array_key_exists('offset', $params) === true) {
+            $this->params['offset'] = intval($params['offset']);
+        }
+    }
+
+    /**
+     * Validate the instances param, need to see if it should exist first
+     *
+     * @param integer $site_id
+     * @param integer $content_id
+     *
+     * @return boolean
+     */
+    protected function validateInstances($site_id, $content_id)
+    {
+        // TODO: Implement validateInstances() method.
+    }
+
+    /**
+     * Add a new item
+     *
+     * @return array|FALSE Ids for new environment vars or FALSE if the request failed
+     */
+    protected function add()
+    {
+        // TODO: Implement add() method.
+    }
+
+    /**
+     * Edit the item
+     *
+     * @return array|FALSE Ids for new environment vars or FALSE if the request failed
+     * @throws Exception
+     */
+    protected function edit()
+    {
+        // TODO: Implement add() method.
+    }
+
+    /**
+     * Make a structural change to the page
+     *
+     * @return array|FALSE An array of the environment vars to set or FALSE upon error
+     */
+    protected function structure()
+    {
+        $this->model = new Dlayer_DesignerTool_ContentManager_Column_SubTool_Settings_Model();
+
+        //$this->currentValues();
+
+        //$this->width();
+
+        //$this->offset()
+
+        return $this->returnIds();
+    }
+
+    /**
+     * Generate the return ids array
+     *
+     * @return array
+     */
+    protected function returnIds()
+    {
+        return array(
+            array(
+                'type' => 'page_id',
+                'id' => $this->page_id,
+            ),
+            array(
+                'type' => 'row_id',
+                'id' => $this->row_id,
+            ),
+            array(
+                'type' => 'column_id',
+                'id' => $this->column_id,
+            ),
+            array(
+                'type' => 'tool',
+                'id' => 'Column',
+            ),
+            array(
+                'type' => 'tab',
+                'id' => 'settings',
+                'sub_tool' => 'Settings'
+            )
+        );
+    }
+}
