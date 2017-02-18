8019bacadeba5ee2e70ca3c2d91ef76fd071a4c4
Dean Blackborough <dean@g3d-development.com>
2017-02-18 15:12:24 +0000

    Typography
    - Added typography sub tool

--- library/Dlayer/Ribbon/Handler/Content.php
+++ library/Dlayer/Ribbon/Handler/Content.php
@@ -550,6 +550,11 @@ class Dlayer_Ribbon_Handler_Content
                 $data = $ribbon_heading_date->viewData($this->toolParams($tool));
                 break;
 
+            case 'typography':
+                $ribbon_heading_date = new Dlayer_DesignerTool_ContentManager_HeadingDate_SubTool_Typography_Ribbon();
+                $data = $ribbon_heading_date->viewData($this->toolParams($tool));
+                break;
+
             default:
                 $data = false;
                 break;
