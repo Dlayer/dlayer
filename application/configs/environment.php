c519bf815ed89628764dc2b50a05acac05a6b9f6
Dean Blackborough <dean@g3d-development.com>
2017-02-18 18:19:21 +0000

    Envronment
    - Set environment to development
    - Updated md syntax in changelog

--- application/configs/environment.php
+++ application/configs/environment.php
@@ -10,10 +10,10 @@
 * production, development, testing or staging
 */
 
-$environment = 'production';
-//$environment = 'development';
+//$environment = 'production';
+$environment = 'development';
 
 // Version number for current release
-$version_no = 'v1.13';
+$version_no = 'v1.14';
 // Release date for current release
-$version_release_date = '18th February 2017';
+$version_release_date = 'In Progress';
